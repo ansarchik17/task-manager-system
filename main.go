62b85d183fdfbec3aa8b11257847c0d6ee33fd8e
Ansar <kelesansar62@gmail.com>
2026-01-15 00:33:31 +0500

    changed main go file

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"task-manager/config"
 	handler "task-manager/handlers"
 	"task-manager/repositories"
-	"task-manager/service"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -31,18 +28,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	repo := repositories.NewInMemoryTaskRepository()
-
-	taskService := service.NewTaskService(repo)
-
-	taskHandler := handler.NewTaskHandler(taskService)
-
-	http.HandleFunc("/tasks/create", taskHandler.CreateTask)
-	http.HandleFunc("/tasks", taskHandler.GetTasks)
-	http.HandleFunc("/tasks/delete", taskHandler.DeleteTask)
-
-	fmt.Println("Server running on :8080")
-	http.ListenAndServe(":8080", nil)
+	tasksRepository := repositories.NewTaskRepository(connection)
+	taskHandler := handler.NewTaskHandler(tasksRepository)
+	r.POST("/tasks", taskHandler.CreateTask)
+	r.GET("/tasks", taskHandler.GetAllTasks)
+	r.GET("/task/:id", taskHandler.GetTaskById)
+	r.Run(":8070")
 }
 
 func connectToDb() (*pgxpool.Pool, error) {
@@ -66,7 +57,7 @@ func loadConfig() error {
 		viper.SetDefault("APP_HOST", ":8070")
 	}
 	if err := viper.BindEnv("DB_CONNECTION_STRING"); err != nil {
-		viper.SetDefault("DB_CONNECTION_STRING", "postgres://task-manager-system:ansar2007+A@localhost:5432/ozinshe?sslmode=disable")
+		viper.SetDefault("DB_CONNECTION_STRING", "postgres://postgres:ansar2007+A@localhost:5430/task-manager-system?sslmode=disable")
 	}
 	if err := viper.BindEnv("JWT_SECRET_KEY"); err != nil {
 		viper.SetDefault("JWT_SECRET_KEY", "supersecretkey")
