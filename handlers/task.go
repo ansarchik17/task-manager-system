79633101f56f1b7b53f0abbb2f31eb983c3e4889
Ansar <kelesansar62@gmail.com>
2026-01-15 01:27:10 +0500

    обработчики

--- handlers/task.go
+++ handlers/task.go
@@ -66,101 +66,51 @@ func (handler *TaskHandler) GetTaskById(c *gin.Context) {
 	c.JSON(http.StatusOK, task)
 }
 
-// func (handler *TaskHandler) PostTask(c *gin.Context) {
-// 	var request models.CreateTaskRequest
+func (handler *TaskHandler) UpdateTask(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("Invalid id"))
+		return
+	}
 
-// 	err := c.BindJSON(&request)
+	_, err = handler.taskRepo.FindTaskById(c, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError(err.Error()))
+		return
+	}
 
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("Could not bind json body"))
-// 		return
-// 	}
+	var request models.UpdateTaskDTO
+	err = c.Bind(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("Could not bind json body"))
+		return
+	}
 
-// 	if request.Title == "" {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("title is required"))
-// 		return
-// 	}
+	updatedTask := models.Task{
+		Title:  request.Title,
+		Status: request.Status,
+	}
 
-// 	task, err := handler.service.Create(request.Title)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, models.NewApiError(err.Error()))
-// 		return
-// 	}
-// 	c.JSON(http.StatusCreated, task)
-// }
+	err = handler.taskRepo.Update(c, id, updatedTask)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "updated successfully"})
+}
 
-// func (handler *TaskHandler) GetTask(c *gin.Context) {
-// 	tasks := handler.service.GetTasks()
-// 	c.JSON(http.StatusOK, tasks)
-// }
+func (handler *TaskHandler) DeleteTask(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("Invalid id"))
+		return
+	}
 
-// func (handler *TaskHandler) GetTaskById(c *gin.Context) {
-// 	idStr := c.Param("id")
-
-// 	id, err := strconv.Atoi(idStr)
-
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("Invalid id"))
-// 		return
-// 	}
-
-// 	task, err := handler.service
-// }
-
-// func (handler *TaskHandler) DeleteTask(c *gin.Context) {
-// 	idStr := c.Param("id")
-// 	id, err := strconv.Atoi(idStr)
-
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("Invalid id"))
-// 		return
-// 	}
-
-// 	err = handler.service.DeleteTask(id)
-
-// 	if err != nil {
-// 		c.JSON(http.StatusNotFound, models.NewApiError("Task does not found"))
-// 		return
-// 	}
-// 	c.Status(http.StatusNoContent)
-// }
-
-// func (handler *TaskHandler) PutTask(c *gin.Context) {
-
-// 	idStr := c.Param("id")
-// 	id, err := strconv.Atoi(idStr)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("invalid id"))
-// 		return
-// 	}
-
-// 	// 2. JSON body
-// 	var req models.UpdateTaskDTO
-// 	if err := c.BindJSON(&req); err != nil {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("invalid json body"))
-// 		return
-// 	}
-
-// 	// 3. PUT validation (қатаң)
-// 	if req.Title == "" || req.Status == "" {
-// 		c.JSON(http.StatusBadRequest, models.NewApiError("title and status are required"))
-// 		return
-// 	}
-
-// 	// 4. Service
-// 	updatedTask, err := handler.service.UpdateTask(id, req.Title, req.Status)
-// 	if err != nil {
-// 		if err.Error() == "task not found" {
-// 			c.JSON(http.StatusNotFound, models.NewApiError(err.Error()))
-// 			return
-// 		}
-// 		c.JSON(http.StatusBadRequest, models.NewApiError(err.Error()))
-// 		return
-// 	}
-// 	// 5. Response
-// 	c.JSON(http.StatusOK, updatedTask)
-// }
-
-// func (hander *TaskHandler) PatchTask(c *gin.Context) {
-
-// }
+	err = handler.taskRepo.Delete(c, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.NewApiError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "task deleted successfully"})
+}
